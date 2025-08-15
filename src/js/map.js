55af176c23a05a6155713259c35603ed74da5e61
Evelina Olsson <evol2401@student.miun.se>
2025-08-16 01:18:08 +0200

    Added map with dark mode theme

---
+++ src/js/map.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const map = L.map('map').setView([20, 0], 2);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
+  subdomains: 'abcd',
+  maxZoom: 19
+}).addTo(map);
+
+
